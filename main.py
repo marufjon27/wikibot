38d35d9ad989dfbfc503b3a0d8c3d42c912e43f8
marufjon27 <109968897+marufjon27@users.noreply.github.com>
2022-07-29 18:16:56 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import wikipedia
+from aiogram import Bot,Dispatcher,executor,types
+wikipedia.set_lang('uz')
+API_TOKEN = '5441949759:AAFxEkRBUdTF4B1peuG-v6lUETPSe-1VTqU'
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+@dp.message_handler(commands='start')
+async def welcome(message: types.Message):
+    await message.answer("Salom wikipedia botiga xush kelibsiz!")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+
+    try:
+        respond = wikipedia.summary(message.text.lower())
+        await message.reply(respond)
+    except:
+
+        await message.reply("Bu mavzuga oid maqola topilmadi!")
+
+if __name__=="__main__":
+    executor.start_polling(dp,skip_updates=True)
+
